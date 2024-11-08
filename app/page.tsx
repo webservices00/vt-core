804e1d21b48c62803b3bec01075393eee58c22f9
webservices00 <webservices00@hotmail.com>
2024-11-08 20:47:10 +0100

    test main #1

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,16 @@
 export default function Home() {
   return (
     <main>
-      <div id="MainHeaderContainer" className="text-2xl text-emerald-400">
-      VT-CORE/WEB
-      <a href="/home">/REDIRECT/HOME/LOOP</a>  
+      <div id="MainHeaderContainer" className="text-2xl text-emerald-700">
+      <a href="/home">
+        VT-CORE/WEB
+      </a>  
       </div>
+      <section id="MainBody1" className="font-semibold">
+        <div id="MB1-1">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos eaque consequatur ratione itaque et, iure porro dolorem tempora est, quaerat suscipit blanditiis nam libero sunt excepturi vel doloribus praesentium repellendus.
+        </div>
+      </section>
     </main>
   );
 }
